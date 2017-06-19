084e37d837e072c6c8487406efb1770a62d8cbdc
inner-muscle <tsuruua1223@gmail.com>
2017-06-19 17:13:15 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,5 +1,19 @@
-window.alert('こんにちは世界！');
+var number = Math.floor(Math.random()*3);
+var answer = parseInt(window.prompt('数当てゲーム。0～2の数字を入力してください！'));
 
-console.log('こんにちは世界！');
+var message;
+if(answer === number) {
+  message = 'あたり！';
+  )else if (answer < number){
+    message = '残念！もっと大きいです！';
+    )else if (number < answer){
+      message = '残念！もっと小さいです！';
+      )else{
+        message = '0～3の数字を当ててください!';
+      }
+      
+      document.getElementByid('choice').textContent = message;
+        
+       
 
 
