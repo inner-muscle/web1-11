48f26091c7937a2f5e68dc1cdc25bfbbdab431d2
inner-muscle <tsuruua1223@gmail.com>
2017-06-19 17:31:03 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,15 +1,17 @@
+
+
 var number = Math.floor(Math.random()*3);
 var answer = parseInt(window.prompt('数当てゲーム。0～2の数字を入力してください！'));
 
 var message;
 if(answer === number) {
   message = 'あたり！';
-  )else if (answer < number){
+}else if (answer < number){
    message = '残念！もっと大きいです！';
-  )else if (number < answer){
+}else if (number < answer){
     message = '残念！もっと小さいです！';
-  )else{
-    message = '0～3の数字を当ててください!';
+}else{
+    message = '0～3の数字を入力してください!';
   }
       
 
